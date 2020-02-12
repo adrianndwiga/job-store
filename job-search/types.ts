10faa140d22b0e22fc182531fe4c55bffe84bcc5
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2020-02-12 14:59:54 +0000

    adding job search and detail config

---
+++ job-search/types.ts
@@ -0,0 +1,16 @@
+export interface JobDetailConfig {
+    title: string
+    company: string
+    salary: string
+    link: string
+    location: string
+}
+
+export interface Config {
+    title: string
+    baseUrl: string
+    url: string
+    cookie?: string
+    result: string
+    detail: JobDetailConfig
+}
