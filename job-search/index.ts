389c8ad8db6450ac8e30b3db28f1523c4b311f10
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2020-02-12 22:00:29 +0000

    adding job search config functionality

--- job-search/index.ts
+++ job-search/index.ts
@@ -1,4 +1,5 @@
 import { Job } from "../types"
+import { Config } from "./types"
 
 export class Store {
     private static JobSearchCollection: string = 'job-search'
@@ -18,5 +19,22 @@ export class Store {
     async getJobs(): Promise<Job[]> {
         const collection = await this.store.collection(Store.JobSearchCollection).get()
         return collection.docs.map(x => x.data()) as Job[]
+    }
+}
+
+// tslint:disable-next-line: max-classes-per-file
+export class ConfigStore {
+    private static JobSearchConfigCollection: string = 'job-search-config'
+
+    constructor(private readonly store : FirebaseFirestore.Firestore) {
+    }
+
+    private addConfig(key: string, config: Config): void {
+        const configRef = this.store.collection(ConfigStore.JobSearchConfigCollection).doc(key)
+        configRef.set(config)
+    }
+
+    async addConfigs(configItems: [{key: string, config: Config}]): Promise<void> {
+        configItems.forEach(item => this.addConfig(item.key, item.config))
     }
 }
